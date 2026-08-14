a8e781d023339a1692877738c88edde98f2b52cb
RE <ron.earle@gmail.com>
2026-08-14 09:03:38 +0200

    Fixed image return, variable return in win_vs_losses.

--- game/templatetags/warrior_wins_losses_tag.py
+++ game/templatetags/warrior_wins_losses_tag.py
@@ -7,14 +7,33 @@ from game.wins_losses import WinsVsLossesView
 register = template.Library()
 
 
-
+#    TAGS take a template as a snippet of html to form the tag and is inserted into the template
+#    page where you want to use the tag. In this case we are loading a db into a table.
 #    TAGS ARE loaded into templates as Django templates {% load warrior_wins_losses_tag %} 
 #    Then in the template use the function as {% render_character_table %}
 @register.inclusion_tag("game/includes/character_table.html")
 def render_character_table(characters):
+    # warrior = Warrior.objects.filter(id__in=[char.id for char in characters])
+    # print(warrior)
+    print("TYPE:", type(characters))
+
+    for c in characters:
+        print(
+            "ID:", c.id,
+            "TYPE:", type(c),
+            "CLASS:", c.__class__.__name__,
+        )
+    print("characters:", characters)
+
+    ids = [char.id for char in characters]
+    print("ids:", ids)
+
+    warriors = Warrior.objects.filter(id__in=ids)
+    print("warriors:", list(warriors))
+
     return {
-        "characters": Warrior.objects.filter(id__in=[char.id for char in characters]),
-    }
+        "characters": warriors,
+    }   
 
 @register.simple_tag(takes_context=True)
 def get_date(context):
